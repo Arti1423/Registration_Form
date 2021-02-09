6cde6018b0faec6f506d6b9ef1874bda982d1ccd
Arti1423 <arti68057@gmail.com>
2021-02-10 00:11:54 +0530

    created registration file

---
+++ registration.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>"Application Form"</title>
+	</head>
+	<body>
+		<div class="container">
+			<div class="header">
+				<h2>Application form</h2>
+			</div>
+			<form action="" . method="post">
+				<div>
+					<br>
+					<label for="name">Name of Applicant : </label>
+					<br>
+					<input type="text" name="name">
+					<br>
+				</div>
+
+				<div>
+					<br>
+					<label for="address">Address</label>
+					<br>
+					<input type="text" name="address">
+					<br>
+				</div>
+				<div>
+					<br>
+					<label for="email">Email : </label>
+					<br>
+					<input type="email" name="email">
+					<br>
+				</div>
+				<div>
+					<br>
+					<label for="phone">Phone Number : </label>
+					<br>
+					<input type="number" name="phonenumber">
+					<br>
+				</div>
+				<br>
+				<button type="submit" name="create" value="Signup">Submit </button>
+			</form>
+
+		</div>
+
+	</body>
+
+</html>
